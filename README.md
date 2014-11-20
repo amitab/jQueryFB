5f7126c2862b0704258e17e5e8d6b84bf9fb2fa6
Amitabh Das <amitab.das@outlook.com>
2014-11-20 19:22:42 +0530

    extended $.fn

--- README.md
+++ README.md
@@ -3,24 +3,30 @@
 To initialize: 
 
 ```javascript
-$fb.init ({
+$(document).fb.init ({
     appId                             : 'xxxxxxxxxxxxx',        // your app id
     version                           : 'v2.1',                 // version of Facebook SDK
     scope                             : 'email',                // the data you need access to
     autocheck                         : true,                   // autocheck login on init()
     status                            : true,                   // optional. default is true
     xfbml                             : true,                   // optional. default is true
+    preInitCallback                   : function () {
+                                            // Optional. Fires before FB.init() is called
+                                        },
+    postInitCallback                   : function () {
+                                            // Optional. Fires after FB.init() is called
+                                        },
     loginSuccessCallback              : function(response) {
                                             // Optional. 
-                                            // You can pass a login success handler in the $fb.login() method as well
+                                            // You can pass a login success handler in the $(document).fb.login() method as well
                                         },
     loginFailCallback                 : function(response) {
                                             // Optional
-                                            // You can pass a login fail handler in the $fb.login() method as well
+                                            // You can pass a login fail handler in the $(document).fb.login() method as well
                                         },
     logoutCallback                    : function(response) {
                                             // Optional
-                                            // You can pass a logout handler in the $fb.logout() method as well
+                                            // You can pass a logout handler in the $(document).fb.logout() method as well
                                         }
 });
 ```
@@ -28,14 +34,14 @@ $fb.init ({
 To Login a user:
 
 ```javascript
-$fb.login (
+$(document).fb.login (
     function (response) {
-        // Optional only if you have set login success handler in $fb.init() method. 
-        // Here you can override the loginSuccessCallback set during $fb.init() method
+        // Optional only if you have set login success handler in $(document).fb.init() method. 
+        // Here you can override the loginSuccessCallback set during $(document).fb.init() method
     },
     function (response) {
         // Optional
-        // Here you can override the loginSuccessCallback set during $fb.init() method
+        // Here you can override the loginSuccessCallback set during $(document).fb.init() method
     }
 );
 ```
@@ -43,9 +49,9 @@ $fb.login (
 To Logout a user:
 
 ```javascript
-$fb.logout (
+$(document).fb.logout (
     // Optional callback function
-    // Here you can override the logout callback set durin $fb.init() method
+    // Here you can override the logout callback set durin $(document).fb.init() method
     function (response) {
         // logout callback
     }
@@ -55,7 +61,7 @@ $fb.logout (
 To fetch user information:
 
 ```javascript
-$fb.api({
+$(document).fb.api({
         path: '/me',
         method: 'get', // default is get. Can use post, delete
         params: { fields: 'last_name' } // parameters to pass to Graph API call
--- jQueryFB.js
+++ jQueryFB.js
@@ -1,136 +1,155 @@
-var $fb = $fb || {};
+(function ($) {
+    
+    $.fn.fb = {};
+    
+    $.fn.fb.init = function (config) {
 
-$fb.init = function (config) {
+        var getValue = function (intendedValue, defaultValue) {
+                // returns intendedValue if it is defined else it returns defaultValue
+                return typeof (intendedValue) === 'undefined' ? defaultValue : intendedValue;
+            },
 
-    var getValue = function (intendedValue, defaultValue) {
-            // returns intendedValue if it is defined else it returns defaultValue
-            return typeof (intendedValue) === 'undefined' ? defaultValue : intendedValue;
-        },
-        
-        // calls intendedFunction if defined else calls defaultFunction
-        callIfAvailiable = function (response, intendedFunction, defaultFunction) {
-            if (typeof (intendedFunction) === 'undefined') {
-                defaultFunction(response);
-            } else {
-                intendedFunction(response);
-            }
-        },
-        
-        scope = typeof (config.scope) === 'undefined' ? {} : config.scope,
-        
-        autocheck = typeof (config.autocheck) === 'undefined' ? true : config.autocheck,
-        
-        settings = {
-            appId      : config.appId,
-            status     : getValue(config.status, true),
-            xfbml      : getValue(config.xfbml, true),
-            version    : getValue(config.version, 'v2.0')
-        },
-        
-        defaultFailCallback = function (response) {
-            window.console.log('Error: ' + getValue(response.error, 'No response recieved'));
-        },
-        
-        defaultLogoutCallback = function () {
-            window.console.log('User Logged out');
-        },
-        
-        loginSuccessCallback = config.loginSuccessCallback,
-        
-        logoutCallback = getValue(config.logoutCallback, defaultLogoutCallback),
-        
-        loginFailCallback = getValue(config.loginFailCallback, defaultFailCallback),
-        
-        statusChangeCallback = function (response, successCallback, failCallback) {
+            // calls intendedFunction if defined else calls defaultFunction
+            callIfAvailiable = function (intendedFunction, defaultFunction, response) {
+                if (typeof (intendedFunction) === 'undefined') {
+                    
+                    if (typeof (defaultFunction) === 'undefined') {
+                        return;
+                    } else {
+                        defaultFunction(response);
+                    }
+                    
+                } else {
+                    intendedFunction(response);
+                }
+            },
 
-            if (response.status === 'connected') {
-                callIfAvailiable(response, successCallback, loginSuccessCallback);
-            } else {
-                callIfAvailiable(response, failCallback, loginFailCallback);
-            }
+            scope = typeof (config.scope) === 'undefined' ? {} : config.scope,
 
-        };
+            autocheck = typeof (config.autocheck) === 'undefined' ? true : config.autocheck,
 
-    // Asynchronously load the facebook javascript sdk
-    $.ajaxSetup({ cache: true });
-    $.getScript('//connect.facebook.net/en_UK/all.js', function () {
-        // todo: add before loading SDK event
-        FB.init(settings);
-        // todo: add after loading SDK event
-        
-        // expose FB
-        $fb.FB = FB;
+            settings = {
+                appId      : config.appId,
+                status     : getValue(config.status, true),
+                xfbml      : getValue(config.xfbml, true),
+                version    : getValue(config.version, 'v2.0')
+            },
 
-        // Login dialouge is popped open if user isn't logged in
-        $fb.login = function (successCallback, failCallback) {
-            FB.getLoginStatus(function (response) {
+            defaultFailCallback = function (response) {
+                window.console.log('Error: ' + getValue(response.error, 'No response recieved'));
+            },
+
+            defaultLogoutCallback = function () {
+                window.console.log('User Logged out');
+            },
+
+            loginSuccessCallback = config.loginSuccessCallback,
+
+            logoutCallback = getValue(config.logoutCallback, defaultLogoutCallback),
+
+            loginFailCallback = getValue(config.loginFailCallback, defaultFailCallback),
+            
+            preInitCallback = getValue(config.preInitCallback),
+            
+            postInitCallback = getValue(config.postInitCallback),
+
+            statusChangeCallback = function (response, successCallback, failCallback) {
+
                 if (response.status === 'connected') {
-                    callIfAvailiable(response, successCallback, loginSuccessCallback);
+                    callIfAvailiable(successCallback, loginSuccessCallback, response);
                 } else {
-                    FB.login(function (response) {
+                    callIfAvailiable(failCallback, loginFailCallback, response);
+                }
+
+            };
+
+        // Asynchronously load the facebook javascript sdk
+        $.ajaxSetup({ cache: true });
+        $.getScript('//connect.facebook.net/en_UK/all.js', function () {
+            (function (FB) {
+                
+                callIfAvailiable(preInitCallback);
+                
+                FB.init(settings);
+                
+                callIfAvailiable(postInitCallback);
+
+                // expose FB
+                $.fn.fb.FB = FB;
+
+                // Login dialouge is popped open if user isn't logged in
+                $.fn.fb.login = function (successCallback, failCallback) {
+                    FB.getLoginStatus(function (response) {
+                        if (response.status === 'connected') {
+                            callIfAvailiable(successCallback, loginSuccessCallback, response);
+                        } else {
+                            FB.login(function (response) {
+                                statusChangeCallback(response, successCallback, failCallback);
+                            }, {
+                                scope: scope,
+                                return_scopes: true
+                            });
+                        }
+                    });
+                };
+
+                // user is logged out of App
+                $.fn.fb.logout = function (callback) {
+                    FB.getLoginStatus(function (response) {
+                        if (response && response.status === 'connected') {
+                            FB.logout(function (response) {
+                                callIfAvailiable(callback, logoutCallback, response);
+                            });
+                        }
+                    });
+                };
+
+                // Checks login status
+                $.fn.fb.checkLoginStatus = function (successCallback, failCallback) {
+                    FB.getLoginStatus(function (response) {
                         statusChangeCallback(response, successCallback, failCallback);
-                    }, {
-                        scope: scope,
-                        return_scopes: true
+                    });
+                };
+
+                // Method to use FB.ui() call
+                $.fn.fb.ui = function (params, successCallback, failCallback) {
+                    FB.ui(params, function (response) {
+                        if (response && !response.error_code) {
+                            successCallback(response);
+                        } else {
+                            callIfAvailiable(failCallback, defaultFailCallback, response);
+                        }
+                    });
+                };
+
+                // Method to use the FB.api() call
+                $.fn.fb.api = function (parameters, successCallback, failCallback) {
+                    // path, method, params
+
+                    if (typeof (parameters.method) === 'undefined') {
+                        parameters.method = 'get';
+                    }
+
+                    FB.api(parameters.path, parameters.method, parameters.params, function (response) {
+                        if (!response || response.error) {
+                            callIfAvailiable(failCallback, defaultFailCallback, response);
+                        } else {
+                            successCallback(response);
+                        }
+                    });
+
+                };
+
+                // Auto check login status after SDK is loaded
+                if (autocheck) {
+                    FB.getLoginStatus(function (response) {
+                        statusChangeCallback(response);
                     });
                 }
-            });
-        };
+            }(FB));
+        });
 
-        // user is logged out of App
-        $fb.logout = function (callback) {
-            FB.getLoginStatus(function (response) {
-                if (response && response.status === 'connected') {
-                    FB.logout(function (response) {
-                        callIfAvailiable(response, callback, logoutCallback);
-                    });
-                }
-            });
-        };
+    };
 
-        // Checks login status
-        $fb.checkLoginStatus = function (successCallback, failCallback) {
-            FB.getLoginStatus(function (response) {
-                statusChangeCallback(response, successCallback, failCallback);
-            });
-        };
-
-        // Method to use FB.ui() call
-        $fb.ui = function (params, successCallback, failCallback) {
-            FB.ui(params, function (response) {
-                if (response && !response.error_code) {
-                    successCallback(response);
-                } else {
-                    callIfAvailiable(response, failCallback, defaultFailCallback);
-                }
-            });
-        };
-
-        // Method to use the FB.api() call
-        $fb.api = function (parameters, successCallback, failCallback) {
-            // path, method, params
-
-            if (typeof (parameters.method) === 'undefined') {
-                parameters.method = 'get';
-            }
-
-            FB.api(parameters.path, parameters.method, parameters.params, function (response) {
-                if (!response || response.error) {
-                    callIfAvailiable(response, failCallback, defaultFailCallback);
-                } else {
-                    successCallback(response);
-                }
-            });
-
-        };
-        
-        // Auto check login status after SDK is loaded
-        if (autocheck) {
-            FB.getLoginStatus(function (response) {
-                statusChangeCallback(response);
-            });
-        }
-    });
-
-};
+}(jQuery));
 
